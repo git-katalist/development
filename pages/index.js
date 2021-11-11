e6ae066f4e034892381addabcf0f7a96c934b0ea
tsots <mosalatsotetsi@gmail.com>
2021-11-11 09:09:28 +0200

    folder cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Hero from '../components/hero/Hero'
 
 export default function Home() {
 return (
 	<>
 		<h1>Hello today</h1>
-<Hero/>
 		</>
 	)
 }
